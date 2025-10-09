947f809ae3979dcda6b2f42ad27f7658d25d2fff
Sreenadh T C <kesav.tc8@gmail.com>
2025-10-09 15:57:36 +0530

    use env in the dev.exs file for creds

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,7 @@
 import Config
 
-config :ex_aws,
+config :ex_aws, :athena,
   debug_requests: true,
-  access_key_id: "abcd",
-  secret_access_key: "1234",
-  region: "us-east-1"
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION", "us-west-1")
