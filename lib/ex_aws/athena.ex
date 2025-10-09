c080717572c2d50922e3e6fe15d55b69ec66ef5e
Sreenadh T C <kesav.tc8@gmail.com>
2025-10-09 15:58:05 +0530

    add exaws utils for camelize, add doc and fix the json version for header

--- lib/ex_aws/athena.ex
+++ lib/ex_aws/athena.ex
@@ -1,5 +1,6 @@
 defmodule ExAws.Athena do
   alias ExAws.Operation.JSON
+  import ExAws.Utils, only: [camelize: 1, camelize_keys: 2]
 
   @namespace "AmazonAthena"
 
@@ -9,30 +10,37 @@ defmodule ExAws.Athena do
   http://docs.aws.amazon.com/athena/latest/APIReference/API_Operations.html
   """
 
-  @spec start_query_execution(query :: String.t(), opts :: keyword()) :: JSON.t()
-  def start_query_execution(query, opts \\ []) do
-    body = %{
+  @doc """
+  Starts a query execution and returns a unique ID representing that execution.
+
+  Refer: https://docs.aws.amazon.com/athena/latest/APIReference/API_StartQueryExecution.html
+  """
+  @spec start_query_execution(
+          query_string :: String.t(),
+          result_configuration :: map(),
+          opts :: keyword()
+        ) :: JSON.t()
+  def start_query_execution(query_string, result_configuration, _opts \\ []) do
+    data = %{
       "ClientRequestToken" => random_string(64),
-      "QueryString" => query
+      "QueryString" => query_string,
+      "ResultConfiguration" => camelize_keys(result_configuration, deep: true)
     }
 
-    request(op, data, opts)
+    request(:start_query_execution, data)
   end
 
   defp request(op, data, opts \\ %{}) do
-    operation =
-      op
-      |> Atom.to_string()
-      |> Macro.camelize()
+    operation = op |> to_string() |> camelize()
 
     JSON.new(
       :athena,
       Map.merge(
         %{
-          data: data,
+          data: Jason.encode!(data),
           headers: [
             {"x-amz-target", "#{@namespace}.#{operation}"},
-            {"content-type", "application/x-amz-json-1.0"}
+            {"content-type", "application/x-amz-json-1.1"}
           ]
         },
         opts
